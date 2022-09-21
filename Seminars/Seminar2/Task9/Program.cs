44a449af787ebdac229a19c66eef2c4352f656a6
Elizaveta <badgerart@mail.ru>
2022-09-21 15:48:12 +0300

    task9

--- Seminars/Seminar2/Task9/Program.cs
+++ Seminars/Seminar2/Task9/Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Console.Clear();
 int number = new Random ().Next(10,100);
 
 Console.WriteLine(number);
+
+int firstDigit = number % 10;
+int lastDigit = number / 10;
+
+if (firstDigit > lastDigit)
+    Console.WriteLine(firstDigit);
+else
+    Console.WriteLine(lastDigit);
