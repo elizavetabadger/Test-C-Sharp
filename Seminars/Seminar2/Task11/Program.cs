cc28424490e8cd946bf5970bc636f5713a2dff3e
Elizaveta <badgerart@mail.ru>
2022-09-21 16:04:23 +0300

    task11

---
+++ Seminars/Seminar2/Task11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int number = new Random ().Next(100,1000);
+
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+
+Console.WriteLine($"{firstDigit}{lastDigit}");
