ec7532de2e3bc38ae7a7d6f06f4c5fe6f2780478
Elizaveta <badgerart@mail.ru>
2022-09-21 16:20:18 +0300

    перемен измен

--- Seminars/Seminar2/Task12/Program.cs
+++ Seminars/Seminar2/Task12/Program.cs
@@ -1,15 +1,15 @@
-﻿//Console.Clear();
+﻿Console.Clear();
 
 Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
 
-if (a % b == 0)
+if (b % a == 0)
 {
     Console.WriteLine($"{b} кратно {a}");
 }
 else
 {
-    Console.WriteLine($"остаток от деления {a % b}");
+    Console.WriteLine($"остаток от деления {b % a}");
 }
