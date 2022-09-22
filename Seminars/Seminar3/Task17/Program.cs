2b5ed00cd316756f7995508c0a865d55de54ff06
Elizaveta <badgerart@mail.ru>
2022-09-22 20:34:27 +0300

    s3_t17

---
+++ Seminars/Seminar3/Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿//Console.Clear();
+Console.Write("Введите X: ");
+int X = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X > 0) {
+    if (Y > 0) 
+    {
+        Console.WriteLine("1");
+    } 
+    else
+    {
+        Console.WriteLine("4");
+    } 
+}
+else
+{
+    if (Y > 0)
+    {
+        Console.WriteLine("2");
+    }
+    else
+    {
+        Console.WriteLine("3");
+    }
+}
