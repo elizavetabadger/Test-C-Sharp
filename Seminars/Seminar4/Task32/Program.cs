fd2c0649ac247b85a41c15b3afc4c68a0836045a
Elizaveta <badgerart@mail.ru>
2022-09-26 21:07:59 +0300

    s4_t32_33

---
+++ Seminars/Seminar4/Task32/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+Console.Clear();
+int[] array = new int[] {-4,-8,8,2};
+int lenght = array.Length;
+for ( int i = 0; i<=lenght-1; i++)
+{
+    array[i] = -array[i];
+}
+
+PrintArray(array);
+
+
+void PrintArray (int[] array)
+{
+    int lenght = array.Length;
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
---
+++ Seminars/Seminar4/Task33/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+Console.Clear();
+int[] array = new int[] {6,7,19,345, -3, 6};
+
+Console.WriteLine ("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+for (int i=0; i<=array.Length-1; i++)
+{
+    if (array[i]==number)
+    {
+        count = count + 1;
+    }
+    else
+        count = count + 0;
+}
+    Console.WriteLine ("число " + number + " встречалось " + count + " раз");
