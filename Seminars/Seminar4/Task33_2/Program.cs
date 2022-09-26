e59f4067e99fd6baa3e565533dadaeba047e2cdd
Elizaveta <badgerart@mail.ru>
2022-09-26 22:49:58 +0300

    s4_

---
+++ Seminars/Seminar4/Task33_2/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+if (FindNunberInArray(array, num))
+    Console.Write($"число {num} присутствует в массиве");
+else
+    Console.Write($"число {num} отсутсвует в массиве");
+
+bool FindNunberInArray(int[] array, int num)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] == num)
+        {
+            return true;            
+        }
+    }
+    return false;
+}
+
+void FillArray(int[] array)
+{
+    Random randGenegator = new Random();
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = randGenegator.Next(0, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }    
+}
---
+++ Seminars/Seminar4/Task37/Program.cs
@@ -0,0 +1,56 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+Console.Clear();
+int[] array = new int[11];
+
+FillArray(array);
+PrintArray(array);
+int[] newArray = Product(array);
+PrintArray(newArray);
+
+int[] Product(int[] arr)
+{
+    int[] newarr;
+    if (arr.Length % 2 == 0)
+    {
+        newarr = new int[arr.Length/2];
+        for (int i = 0; i < arr.Length/2; i++)
+        {
+            newarr[i] = arr[i]*arr[arr.Length - 1- i];
+        }
+    }
+    else
+    {
+        newarr = new int[arr.Length/2 + 1];
+        for (int i = 0; i < arr.Length/2 + 1; i++)
+        {
+            if (i == arr.Length/2)
+                newarr[i] = arr[i];
+            else 
+                newarr[i] = arr[i]*arr[arr.Length - 1- i];
+            
+        }
+    }
+    return newarr;
+}
+
+void FillArray(int[] array)
+{
+    Random randGenegator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenegator.Next(0, 20);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }    
+}
