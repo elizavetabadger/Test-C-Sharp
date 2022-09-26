5dd073a35b7451f432903d617738631f417cdc95
Elizaveta <badgerart@mail.ru>
2022-09-26 21:17:34 +0300

    s4_t32

--- Seminars/Seminar4/Task32/Program.cs
+++ Seminars/Seminar4/Task32/Program.cs
@@ -2,7 +2,7 @@
 Console.Clear();
 int[] array = new int[] {-4,-8,8,2};
 int lenght = array.Length;
-for ( int i = 0; i<=lenght-1; i++)
+for ( int i = 0; i<=array.Length-1; i++)
 {
     array[i] = -array[i];
 }
