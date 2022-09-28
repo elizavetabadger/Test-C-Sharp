f77f572abc6c914f807483b685bf90c2cd6d0084
Elizaveta <badgerart@mail.ru>
2022-09-28 22:41:58 +0300

    h4_t25

---
+++ Homework4/Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//Console.Clear();
+
+int[] array =  new int[8];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    Random randGenerator = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = randGenerator.Next(0, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
