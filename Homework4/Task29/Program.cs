345c6517d7fa813bfa8c4e0ba0059bc520089fd1
Elizaveta <badgerart@mail.ru>
2022-09-29 22:20:35 +0300

    h4_t29

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -1,29 +1,48 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 //Console.Clear();
+Console.WriteLine("Введите числа через пробел");
+string input = Console.ReadLine()!;
+int[] newArray = ParseToArray(input);
+//int[] newArray = RandomArray();
 
-Console.Write("Введите 8 чисел: ");
-int number = int.Parse(Console.ReadLine()!);
+PrintArray(newArray);
 
-int[] array =  new int[8];
-
-FillArray(array);
-PrintArray(array);
-
-
-void FillArray(int[] array)
+int[] RandomArray()
 {
+    int[] arr = new int[8];
     Random rand = new Random();
     for (int i = 0; i < 8; i++)
     {
-        array[i] = rand.Next(0, 10);
+        arr[i] = rand.Next(0, 100);
     }
+    return arr;
 }
 
-void PrintArray(int[] array)
+void PrintArray(int[] arr)
 {
-    for (int i = 0; i < 8; i++)
+    int count = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(arr[i]);
+        if (i == count - 1)            
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
     }
+    Console.WriteLine();
+}
+
+int[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+ 
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+ 
+    return result;
 }
