a37b8b815e693199e944bbb8782377a4357f183f
Elizaveta <badgerart@mail.ru>
2022-09-29 21:19:28 +0300

    s4

---
+++ Seminars/Seminar5/Task31/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа через пробел");
+ 
+string input = Console.ReadLine()!;
+int[] array = ParseToArray(input);
+ 
+Console.WriteLine($"Положительная сумма: {PositiveSum(array)}");
+Console.Write($"Отрицательная сумма: {NegativeSum(array)}");
+ 
+int[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+ 
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+ 
+    return result;
+}
+ 
+int PositiveSum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count += array[i];
+    }
+ 
+    return count;
+}
+int NegativeSum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+            count += array[i];
+    }
+ 
+    return count;
+}
---
+++ Seminars/Seminar5/Task37/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите метод, который объединяет 2 массива в 1.
+Console.Clear();
+int[] array1 = {5, 18, 123, 6, 2};
+int[] array2 = CreateArray();
+PrintArray(array1);
+PrintArray(array2);
+
+int[] Sum = Unity(array1, array2);
+PrintArray(Sum);
+
+int[] CreateArray()
+{
+    int[] arr = new int[6];
+    Random rand = new Random();
+    for (int i = 0; i < 6; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+    }
+    return arr;
+}
+
+int[] Unity(int[] array1, int[] array2)
+{
+    
+        int[] arraySum = new int[array1.Length + array2.Length];
+        for (int i = 0; i < array1.Length; i++)
+        {
+            arraySum[i] = array1[i];
+        }
+
+        for (int i = array1.Length; i < arraySum.Length; i++)
+        {
+            arraySum[i] = array2[i - array1.Length];
+        }
+        return arraySum;
+    
+    
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(arr[i]);
+        if (i == count - 1)            
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
