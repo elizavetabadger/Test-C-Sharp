9fdfd78b2f26ea51cb01755d784890cceeb54666
Elizaveta <badgerart@mail.ru>
2022-10-06 21:01:45 +0300

    s7_t48

---
+++ Seminars/Seminar7/Task46/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+ 
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите z: ");
+int z = int.Parse(Console.ReadLine()!);
+ 
+int[,,] array = new int[m, n, z];
+FillArray(array);
+PrintArray(array);
+ 
+ 
+ 
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+ 
+ 
+ 
+void FillArray(int[,,] array)
+{
+    Random generator = new Random();
+ 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i,j,k] = generator.Next(0,100);
+            }
+        }
+    }
+}
