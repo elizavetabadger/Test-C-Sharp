403f9f7af3749f86c42154a19c46c5b330826742
Elizaveta <badgerart@mail.ru>
2022-10-06 21:35:54 +0300

    s7_t49

---
+++ Seminars/Seminar7/Task49/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Console.Clear();
+ 
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+ 
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+ChangeArray (array);
+PrintArray(array);
+ 
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                array[i,j] = i + j;
+            }
+        }
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                if ((i%2 == 0) && (j%2 == 0))
+                {
+                    array[i,j] = (int)Math.Pow(array[i,j], 2);
+                }
+            }
+        }
+    }
+} 
+ 
+ void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
